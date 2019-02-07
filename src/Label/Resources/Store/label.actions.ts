82021e144be24e9c3e77e3ab8b3ccb8bb52d5ebd
Arbnor Osmani <arbnori.osmani@gmail.com>
2019-02-07 08:29:07 +0100

    Add Actions on Label Store for all needed modules

--- src/Label/Resources/Store/label.actions.ts
+++ src/Label/Resources/Store/label.actions.ts
@@ -3,6 +3,10 @@ import { Action } from '@ngrx/store';
 export const SET_GENERAL_LABELS = 'SET_GENERAL_LABELS';
 export const SET_SETTINGS_LABELS = 'SET_SETTINGS_LABELS';
 export const SET_POST_TYPE_LABELS = 'SET_POST_TYPE_LABELS';
+export const SET_USER_LABELS = 'SET_USER_LABELS';
+export const SET_POST_LABELS = 'SET_POST_LABELS';
+export const SET_CATEGORY_LABELS = 'SET_CATEGORY_LABELS';
+export const SET_TAG_LABELS = 'SET_TAG_LABELS';
 
 export class SetGeneralLabels implements Action{
     readonly type = SET_GENERAL_LABELS;
@@ -22,4 +26,35 @@ export class SetPostTypesLabels implements Action{
     constructor(public payload: []){}
 }
 
-export type LabelActions = SetGeneralLabels | SetSettingsLabels | SetPostTypesLabels;
+export class SetUserLabels implements Action{
+    readonly type = SET_USER_LABELS;
+
+    constructor(public payload: []){}
+}
+
+export class SetPostLabels implements Action{
+    readonly type = SET_POST_LABELS;
+
+    constructor(public payload: []){}
+}
+
+export class SetCategoryLabels implements Action{
+    readonly type = SET_CATEGORY_LABELS;
+
+    constructor(public payload: []){}
+}
+
+export class SetTagLabels implements Action{
+    readonly type = SET_TAG_LABELS;
+
+    constructor(public payload: []){}
+}
+
+export type LabelActions =
+    SetGeneralLabels |
+    SetSettingsLabels |
+    SetPostTypesLabels |
+    SetUserLabels |
+    SetPostLabels |
+    SetCategoryLabels |
+    SetTagLabels;
--- src/Label/Resources/label.service.ts
+++ src/Label/Resources/label.service.ts
@@ -7,6 +7,10 @@ import * as LabelActions from "./Store/label.actions";
 
 import { FuseTranslationLoaderService } from '../../Shared/@fuse/services/translation-loader.service';
 import 'rxjs/add/operator/toPromise';
+import {SetUserLabels} from "./Store/label.actions";
+import {SetPostLabels} from "./Store/label.actions";
+import {SetCategoryLabels} from "./Store/label.actions";
+import {SetTagLabels} from "./Store/label.actions";
 
 @Injectable({
     providedIn: 'root'
@@ -33,6 +37,14 @@ export class LabelService{
                         this.store.dispatch(new LabelActions.SetPostTypesLabels(response['data']));
                     }else if(module == 'settings'){
                         this.store.dispatch(new LabelActions.SetSettingsLabels(response['data']));
+                    }else if(module == 'post'){
+                        this.store.dispatch(new LabelActions.SetPostLabels(response['data']));
+                    }else if(module == 'category'){
+                        this.store.dispatch(new LabelActions.SetCategoryLabels(response['data']));
+                    }else if(module == 'tag'){
+                        this.store.dispatch(new LabelActions.SetTagLabels(response['data']));
+                    }else if(module == 'users'){
+                        this.store.dispatch(new LabelActions.SetUserLabels(response['data']));
                     }
                 }
             );
